8523d5c81eb6b24f5b39a6445b4e68b9e8d5a1e3
soaumil <soaumil@macbookpro>
2024-12-11 00:01:12 -0500

    A

--- src/Practicee/other.java
+++ src/Practicee/other.java
@@ -1,5 +1,16 @@
 package Practicee;
 
-public class other {
 
+
+	import modifier.aaa; // Import the aaa class from the modifier package
+
+	public class other {
+	    public static void main(String[] args) {
+	        
+	        aaa obj = new aaa(42, "Hello, Private World");
+	        // Access private members through public methods
+	        System.out.println("Number (via method): " + obj.getNumber());
+	        System.out.println("Message (via method): " + obj.getMessage());
+	    }
+	}
 }
